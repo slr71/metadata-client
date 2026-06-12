1d57d192524584763937647f82ec1797080bbc59
Ian McEwen <mian@cyverse.org>
2026-06-12 12:36:08 -0700

    Version 3.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/metadata-client "3.2.1-SNAPSHOT"
+(defproject org.cyverse/metadata-client "3.2.1"
   :description "Client for the metadata service"
   :url "https://github.com/cyverse-de/metadata-client"
   :license {:name "BSD"
