eef348da6a86c98ca215d6f69f452c5c6ec0af1c
Sarah Roberts <sonoran.sarah@gmail.com>
2026-08-17 12:47:15 -0700

    Version 3.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/metadata-client "3.2.2-SNAPSHOT"
+(defproject org.cyverse/metadata-client "3.2.2"
   :description "Client for the metadata service"
   :url "https://github.com/cyverse-de/metadata-client"
   :license {:name "BSD"
